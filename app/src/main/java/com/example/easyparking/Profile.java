9d0078671a548f9c1a24a77015cf031ebce25646
Antonius Indra Dharma Prasetya <anindhpra@gmail.com>
2023-03-11 22:32:57 +0800

    mengatur nama email telp dan logout

--- app/src/main/java/com/example/easyparking/Profile.java
+++ app/src/main/java/com/example/easyparking/Profile.java
@@ -1,65 +1,65 @@
 package com.example.easyparking;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Profile#newInstance} factory method to
- * create an instance of this fragment.
- */
+import androidx.fragment.app.Fragment;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class Profile extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
-    public Profile() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment profile.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Profile newInstance(String param1, String param2) {
-        Profile fragment = new Profile();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    private View view;
+    private TextView namaBesar;
+    private TextView namaKecil;
+    private TextView emailKecil;
+    private TextView telpKecil;
+    private TextView logoutBtn;
+    private FirebaseUser user;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        view = inflater.inflate(R.layout.fragment_profile, container, false);
 
+        namaBesar = view.findViewById(R.id.namaBesar);
+        namaKecil = view.findViewById(R.id.namaKecil);
+        emailKecil = view.findViewById(R.id.emailKecil);
+        telpKecil = view.findViewById(R.id.telpKecil);
+        logoutBtn = view.findViewById(R.id.logoutBtn);
+
+        user = FirebaseAuth.getInstance().getCurrentUser();
+
+        String nama = user.getDisplayName();
+        System.out.println(nama);
+
+        namaBesar.setText(nama);
+        namaKecil.setText(nama);
+        emailKecil.setText(user.getEmail());
+        telpKecil.setText(user.getPhoneNumber());
+
+        logoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(getActivity(), Login.class));
+            }
+        });
+
+        return view;
     }
 }
