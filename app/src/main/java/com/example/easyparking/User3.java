26573330b25cd9be430355d59d95e19cd19e9dc5
Antonius Indra Dharma Prasetya <anindhpra@gmail.com>
2023-03-15 14:01:14 +0800

    tambah class untuk nama mall

---
+++ app/src/main/java/com/example/easyparking/User3.java
@@ -0,0 +1,18 @@
+package com.example.easyparking;
+
+public class User3 {
+    private String namaMall;
+
+    public User3(String namaMall) {
+        this.namaMall = namaMall;
+    }
+
+    public String getNamaMall() {
+        return namaMall;
+    }
+
+    public void modelUser2(String namaMall){
+        this.namaMall = namaMall;
+    }
+
+}
